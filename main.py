6e707ce85046756ce9fb070d2ab8fcf671bd5b3f
thomaspereira94 <thomaspereira94@gmail.com>
2025-12-26 00:39:25 -0500

    Added circle shape and player classes. Draw player on intial screen.

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pygame
-
+from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
     pygame.init()
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
- 
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+
     #infinite loop to keep game window open
     loop_counter = 0
     while loop_counter == 0:
         log_state()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()   
+
+        delta_time = game_clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
